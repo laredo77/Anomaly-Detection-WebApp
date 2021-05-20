fdd1cdadbf4209c4a464877bd8fb32e453316681
Ofir Shtrosberg <ofir.shtrosberg@live.biu.ac.il>
2021-05-20 19:27:59 +0300

    20.5 19:27

--- View/viewJS.js
+++ View/viewJS.js
@@ -1,26 +1,27 @@
-function results(algo) {
-    if (algo == 'LinearReg') {
-        var featueres = require('');
-        var trace1 = {
-            x: [1, 2, 3, 4],
-            y: [10, 15, 13, 17],
-            mode: 'markers'
-        };
-        var data = [ trace1 ];
-        var layout = {
-            title:'Linear Reg'
-        };
-        Plotly.newPlot('myDiv', data, layout);
-    } else {
-        var trace1 = {
-            x: [1, 2, 3, 4],
-            y: [10, 15, 13, 17],
-            mode: 'markers'
-        };
-        var data = [ trace1 ];
-        var layout = {
-            title:'Hybrid Algorithm'
-        };
-        Plotly.newPlot('myDiv', data, layout);
-    }
+function resultLinear() {
+    var trace1 = {
+        x: [1, 2, 3, 4],
+        y: [10, 15, 13, 17],
+        mode: 'markers'
+    };
+    var data = [trace1];
+    var layout = {
+        title: 'Linear Reg'
+    };
+    Plotly.newPlot('myDiv', data, layout);
+}
+
+function resultHybrid() {
+
+    var trace1 = {
+        x: [1, 2, 3, 4],
+        y: [10, 15, 13, 17],
+        mode: 'markers'
+    };
+    var data = [trace1];
+    var layout = {
+        title: 'Hybrid Algorithm'
+    };
+    Plotly.newPlot('myDiv', data, layout);
+
 }
